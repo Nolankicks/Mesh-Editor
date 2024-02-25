8e67c53dc146ba81e4069cafbdc63a820b78a155
Layla <fortblox@gmail.com>
2024-02-25 01:42:21 +0000

    Make block tool functional with resize gizmo, enter to create, escape to cancel

--- code/Editor/BlockTool.cs
+++ code/Editor/BlockTool.cs
@@ -11,7 +11,13 @@ namespace Editor.MeshEditor;
 [Shortcut( "editortool.block", "b" )]
 public class BlockTool : EditorTool
 {
-	private bool _dragging = false;
+	private BBox _box;
+	private BBox _startBox;
+	private BBox _deltaBox;
+	private bool _resizing;
+	private bool _boxCreated;
+	private bool _dragging;
+	private bool _finished;
 	private Vector3 _dragStartPos;
 
 	public override void OnEnabled()
@@ -21,9 +27,82 @@ public class BlockTool : EditorTool
 		Selection.Clear();
 	}
 
+	private GameObject CreateFromBox( BBox box )
+	{
+		var go = new GameObject( true, "Box" );
+		var mc = go.Components.Create<EditorMeshComponent>( false );
+
+		mc.FromBox( box );
+		mc.Enabled = true;
+
+		EditLog( "Create Block", null );
+
+		return go;
+	}
+
 	public override void OnUpdate()
 	{
-		if ( Gizmo.HasHovered )
+		if ( _finished )
+			return;
+
+		if ( _boxCreated && Application.IsKeyDown( KeyCode.Escape ) )
+		{
+			_resizing = false;
+			_dragging = false;
+			_boxCreated = false;
+		}
+
+		if ( _boxCreated )
+		{
+			using ( Gizmo.Scope( "box", 0 ) )
+			{
+				Gizmo.Draw.IgnoreDepth = true;
+				Gizmo.Draw.LineThickness = 2;
+				Gizmo.Draw.Color = Gizmo.Colors.Active;
+				Gizmo.Draw.LineBBox( _box );
+				Gizmo.Draw.Color = Color.White;
+				Gizmo.Draw.ScreenText( $"L: {_box.Size.y:0.#}", Gizmo.Camera.ToScreen( _box.Mins.WithY( _box.Center.y ) ) + Vector2.Down * 32, size: 14 );
+				Gizmo.Draw.ScreenText( $"W: {_box.Size.x:0.#}", Gizmo.Camera.ToScreen( _box.Mins.WithX( _box.Center.x ) ) + Vector2.Down * 32, size: 14 );
+			}
+
+			using ( Gizmo.Scope( "Tool" ) )
+			{
+				Gizmo.Hitbox.DepthBias = 0.01f;
+
+				if ( !Gizmo.HasPressed )
+				{
+					_resizing = false;
+					_deltaBox = default;
+					_startBox = default;
+				}
+
+				if ( Gizmo.Control.BoundingBox( "Resize", _box, out var outBox ) )
+				{
+					if ( !_resizing )
+					{
+						_startBox = _box;
+						_resizing = true;
+					}
+
+					_deltaBox.Maxs += outBox.Maxs - _box.Maxs;
+					_deltaBox.Mins += outBox.Mins - _box.Mins;
+
+					_box.Maxs = _startBox.Maxs + Gizmo.Snap( _deltaBox.Maxs, _deltaBox.Maxs );
+					_box.Mins = _startBox.Mins + Gizmo.Snap( _deltaBox.Mins, _deltaBox.Mins );
+				}
+			}
+
+			if ( Application.IsKeyDown( KeyCode.Enter ) )
+			{
+				var go = CreateFromBox( _box );
+				Selection.Set( go );
+
+				EditorToolManager.CurrentModeName = "object";
+				_finished = true;
+			}
+		}
+
+		if ( Gizmo.HasPressed || Gizmo.HasHovered )
 			return;
 
 		var tr = MeshTrace.Run();
@@ -37,81 +116,76 @@ public class BlockTool : EditorTool
 			}
 		}
 
-		if ( tr.Hit )
+		if ( !tr.Hit )
+			return;
+
+		var r = Rotation.LookAt( tr.Normal );
+		var localPosition = tr.EndPosition * r.Inverse;
+		localPosition = Gizmo.Snap( localPosition, new Vector3( 0, 1, 1 ) );
+		tr.EndPosition = localPosition * r;
+
+		if ( !_dragging )
 		{
-			var r = Rotation.LookAt( tr.Normal );
-			var localPosition = tr.EndPosition * r.Inverse;
-			localPosition = Gizmo.Snap( localPosition, new Vector3( 0, 1, 1 ) );
-			tr.EndPosition = localPosition * r;
-
-			if ( !_dragging )
+			using ( Gizmo.Scope( "Aim Handle", new Transform( tr.EndPosition, Rotation.LookAt( tr.Normal ) ) ) )
 			{
-				using ( Gizmo.Scope( "Aim Handle", new Transform( tr.EndPosition, Rotation.LookAt( tr.Normal ) ) ) )
+				Gizmo.Draw.Color = Color.White;
+				Gizmo.Draw.LineCircle( 0, 2 );
+				Gizmo.Draw.Color = Color.White.WithAlpha( 0.5f );
+				Gizmo.Draw.LineCircle( 0, 3 );
+				Gizmo.Draw.Color = Color.White.WithAlpha( 0.3f );
+				Gizmo.Draw.LineCircle( 0, 6 );
+				Gizmo.Draw.Color = Color.White.WithAlpha( 0.1f );
+				Gizmo.Draw.LineCircle( 0, 12 );
+			}
+		}
+
+		if ( Gizmo.WasLeftMousePressed )
+		{
+			if ( _boxCreated )
+			{
+				CreateFromBox( _box );
+			}
+
+			_dragging = true;
+			_dragStartPos = tr.EndPosition;
+			_boxCreated = false;
+		}
+		else if ( Gizmo.WasLeftMouseReleased && _dragging )
+		{
+			var spacing = Gizmo.Settings.SnapToGrid ? Gizmo.Settings.GridSpacing : 1.0f;
+			var box = new BBox( _dragStartPos, tr.EndPosition );
+
+			if ( box.Size.x >= spacing || box.Size.y >= spacing )
+			{
+				if ( Gizmo.Settings.SnapToGrid )
 				{
-					Gizmo.Draw.Color = Color.White;
-					Gizmo.Draw.LineCircle( 0, 2 );
-					Gizmo.Draw.Color = Color.White.WithAlpha( 0.5f );
-					Gizmo.Draw.LineCircle( 0, 3 );
-					Gizmo.Draw.Color = Color.White.WithAlpha( 0.3f );
-					Gizmo.Draw.LineCircle( 0, 6 );
-					Gizmo.Draw.Color = Color.White.WithAlpha( 0.1f );
-					Gizmo.Draw.LineCircle( 0, 12 );
+					if ( box.Size.x < spacing ) box.Maxs.x += spacing;
+					if ( box.Size.y < spacing ) box.Maxs.y += spacing;
 				}
+
+				const float height = 128;
+				var size = box.Size.WithZ( height );
+				var position = box.Center.WithZ( box.Center.z + (height * 0.5f) );
+				_box = BBox.FromPositionAndSize( position, size );
+				_boxCreated = true;
 			}
 
-			if ( Gizmo.WasLeftMousePressed )
-			{
-				_dragging = true;
-				_dragStartPos = tr.EndPosition;
+			_dragging = false;
+			_dragStartPos = default;
+		}
 
-				Selection.Clear();
-			}
-			else if ( Gizmo.WasLeftMouseReleased && _dragging )
+		if ( _dragging )
+		{
+			using ( Gizmo.Scope( "Rect", 0 ) )
 			{
-				var spacing = Gizmo.Settings.SnapToGrid ? Gizmo.Settings.GridSpacing : 1.0f;
+				Gizmo.Draw.IgnoreDepth = true;
+				Gizmo.Draw.LineThickness = 2;
+				Gizmo.Draw.Color = Gizmo.Colors.Active;
 				var box = new BBox( _dragStartPos, tr.EndPosition );
-
-				if ( box.Size.x >= spacing || box.Size.y >= spacing )
-				{
-					var go = new GameObject( true, "Box" );
-					var mc = go.Components.Create<EditorMeshComponent>( false );
-
-					if ( Gizmo.Settings.SnapToGrid )
-					{
-						if ( box.Size.x < spacing ) box.Maxs.x += spacing;
-						if ( box.Size.y < spacing ) box.Maxs.y += spacing;
-					}
-
-					const float height = 128;
-					var size = box.Size.WithZ( height );
-					var position = box.Center.WithZ( box.Center.z + (height * 0.5f) );
-					mc.FromBox( BBox.FromPositionAndSize( position, size ) );
-					mc.Enabled = true;
-
-					Selection.Set( go );
-
-					EditLog( "Create Block", null );
-				}
-
-				_dragging = false;
-				_dragStartPos = default;
-			}
-
-			using ( Gizmo.Scope( "box", 0 ) )
-			{
-				if ( _dragging )
-				{
-					Gizmo.Draw.IgnoreDepth = true;
-					Gizmo.Draw.LineThickness = 2;
-					Gizmo.Draw.Color = Gizmo.Colors.Active;
-
-					var box = new BBox( _dragStartPos, tr.EndPosition );
-					Gizmo.Draw.LineBBox( box );
-
-					Gizmo.Draw.Color = Color.White;
-					Gizmo.Draw.ScreenText( $"L: {box.Size.y:0.#}", Gizmo.Camera.ToScreen( box.Mins.WithY( box.Center.y ) ) + Vector2.Down * 32, size: 14 );
-					Gizmo.Draw.ScreenText( $"W: {box.Size.x:0.#}", Gizmo.Camera.ToScreen( box.Mins.WithX( box.Center.x ) ) + Vector2.Down * 32, size: 14 );
-				}
+				Gizmo.Draw.LineBBox( box );
+				Gizmo.Draw.Color = Color.White;
+				Gizmo.Draw.ScreenText( $"L: {box.Size.y:0.#}", Gizmo.Camera.ToScreen( box.Mins.WithY( box.Center.y ) ) + Vector2.Down * 32, size: 14 );
+				Gizmo.Draw.ScreenText( $"W: {box.Size.x:0.#}", Gizmo.Camera.ToScreen( box.Mins.WithX( box.Center.x ) ) + Vector2.Down * 32, size: 14 );
 			}
 		}
 	}
